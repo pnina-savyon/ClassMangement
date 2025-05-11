d7fedf0ff536b489089343d8e55341842cb8a52c
pnina-savyon <1644pn@gmail.com>
2025-05-11 23:06:42 +0300

    changes in security - move security from controllers to service : interface + implement in UserService

---
+++ Service/Interfaces/ISecurity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+	public interface ISecurity<T,Y>
+	{
+		string Generate(T user);
+		T Authenticate(Y value);
+		T GetCurrentUser();
+	}
+}
--- Service/Services/UserService.cs
+++ Service/Services/UserService.cs
@@ -1,9 +1,17 @@
-﻿using Common.Dto;
+﻿using AutoMapper;
+using Common.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Repository.Entities;
+using Repository.Entities.Enums;
 using Repository.Interfaces;
 using Service.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +19,22 @@ namespace Service.Services
 {
 	public abstract class UserService<TUserDto> : IService<TUserDto, string> where TUserDto : UserDto
 	{
+		private readonly IRepository<User, string> repository;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly IMapper mapper;
+		private readonly ISecurity<UserDto, UserLogin> _security;
+		private readonly IConfiguration config;
+
+
+		public UserService(IRepository<User, string> repository, IHttpContextAccessor httpContextAccessor, IMapper mapper, ISecurity<UserDto, UserLogin> security, IConfiguration config)
+		{
+			this.repository = repository;
+			_httpContextAccessor = httpContextAccessor;
+			this.mapper = mapper;
+			_security = security;
+			this.config = config;
+		}
+		
 		public abstract TUserDto AddItem(TUserDto item);
 
 		public abstract TUserDto DeleteItem(string id);
@@ -20,5 +44,51 @@ namespace Service.Services
 		public abstract TUserDto GetById(string id);
 
 		public abstract TUserDto UpdateItem(string id, TUserDto item);
+		//security
+
+		public virtual UserDto Authenticate(UserLogin value)
+		{
+			UserDto user= mapper.Map < User, UserDto> (repository.GetAll().FirstOrDefault(x => x.Password == value.Password && x.Email == value.Email));
+			if (user != null)
+				return user;
+			return null;
+		}
+
+		public virtual string Generate(UserDto user)
+		{
+			var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+			var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+			var claims = new[] {
+			new Claim(ClaimTypes.NameIdentifier,user.Name),
+			new Claim(ClaimTypes.Email,user.Email),
+			new Claim(ClaimTypes.PostalCode,user.Password),
+			new Claim(ClaimTypes.Role,user.Role.ToString()),
+
+			};
+			var token = new JwtSecurityToken(config["Jwt:Issuer"], config["Jwt:Audience"],
+				claims,
+				expires: DateTime.Now.AddMinutes(15),
+				signingCredentials: credentials);
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+
+		public virtual UserDto GetCurrentUser()
+		{
+			var identity = _httpContextAccessor.HttpContext?.User?.Identity as ClaimsIdentity;
+			if (identity != null)
+			{
+				var UserClaim = identity.Claims;
+				return new StudentDto()
+				{
+					Name = UserClaim.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+					Email = UserClaim.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+					Password = UserClaim.FirstOrDefault(x => x.Type == ClaimTypes.PostalCode)?.Value,
+					Role = Enum.TryParse(UserClaim.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value, out Roles role) ? role : Roles.None
+
+				};
+			}
+			return null;
+		}
+
 	}
 }
