1186f0b3c74d9a36a0c8d292ace61015623715b0
pnina-savyon <1644pn@gmail.com>
2025-06-16 11:12:11 +0300

    continue the begining of the algorithm

--- Service/SeatAllocation/Interfaces/IConstraintRule.cs
+++ Service/SeatAllocation/Interfaces/IConstraintRule.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Google.OrTools.ConstraintSolver;
+using Google.OrTools.LinearSolver;
+using Repository.Entities;
+using Service.SeatAllocation.Logic.Solver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,6 @@ namespace Service.SeatAllocation.Interfaces
 {
 	public interface IConstraintRule
 	{
+		LinearExpr GetScore(Student student, IntVar studentChairVar, StudentContext context);
 	}
 }
---
+++ Service/SeatAllocation/Logic/Solver/StudentContext.cs
@@ -0,0 +1,44 @@
+﻿using Google.OrTools.Sat;
+using Repository.Entities;
+using Repository.Interfaces;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.SeatAllocation.Logic.Solver
+{
+	public class StudentContext
+	{
+		private readonly IRepository<Chair, int> repositoryChair;
+		private List<Student> students { get;}
+		private List<Chair> chairs { get; }
+		private Dictionary<int, IntVar> studentChairVars { get; }
+		private CpModel model { get; }
+
+		//
+		public StudentContext(IRepository<Chair, int> repositoryChair ,List<Student> students, List<Chair> chairs, CpModel model)
+		{
+			this.repositoryChair = repositoryChair;
+			this.students = students;//?? throw new ArgumentNullException(nameof(students));
+			this.chairs = chairs;// ?? throw new ArgumentNullException(nameof(chairs));
+			this.model = model;//?? throw new ArgumentNullException(nameof(model));
+			this.studentChairVars = new Dictionary<int, IntVar>();
+		}
+
+
+
+		public bool AreChairsNear(int chairId1, int chairId2)
+		{
+			// החישוב לפי Row/Column
+			return false;
+		}
+
+		public Chair GetChairById(int id)
+		{
+			return null;// repositoryChair.GetById(id);
+		}
+	}
+}
