b2b14d5531786b50d3ee6e55fbe35591b7f55594
pnina-savyon <1644pn@gmail.com>
2025-06-16 23:00:46 +0300

    add additional rule

---
+++ Service/SeatAllocation/Logic/Rules/BackPositionOverrideInFront.cs
@@ -0,0 +1,40 @@
+﻿using Google.OrTools.Sat;
+using Repository.Entities;
+using Repository.Entities.Enums;
+using Service.SeatAllocation.Interfaces;
+using Service.SeatAllocation.Logic.Solver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Service.SeatAllocation.Logic.Rules
+{
+	public class BackPositionOverrideInFront : IScoringRule
+	{
+		public LinearExpr GetScore(Student student, IntVar studentChairVar, StudentContext context)
+		{
+			if(student.CurrentChair.IsFront)
+				return LinearExpr.Constant(0);
+
+			int score = (int)(student.Priority * 11);
+			List<LinearExpr> terms = new List<LinearExpr>();
+
+			foreach (Chair chair in context.Chairs)
+			{
+				if (chair.IsFront)
+				{
+					BoolVar isMatch = context.Model.NewBoolVar($"student_{student.Id}_chair_{chair.Id}");
+					context.Model.Add(studentChairVar == chair.Id).OnlyEnforceIf(isMatch);
+					context.Model.Add(studentChairVar != chair.Id).OnlyEnforceIf(isMatch.Not());
+
+					terms.Add(isMatch * score);
+				}
+			}
+
+			return LinearExpr.Sum(terms);
+		}
+	}
+}
